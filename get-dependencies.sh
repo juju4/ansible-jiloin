9afee3079bc4fd0cddc50bfa334b56b2e9440721
juju4 <juju4@users.noreply.github.com>
2016-11-23 23:12:13 -0500

    align meta and get-dependencies

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,5 +10,5 @@ rolesdir=$(dirname $0)/..
 fi
 
 #[ ! -d $rolesdir/freedomofpress.jitsi-meet ] && git clone https://github.com/freedomofpress/ansible-role-jitsi-meet $rolesdir/freedomofpress.jitsi-meet
-[ ! -d $rolesdir/freedomofpress.jitsi-meet ] && git clone https://github.com/juju4/ansible-role-jitsi-meet $rolesdir/freedomofpress.jitsi-meet
+[ ! -d $rolesdir/freedomofpress.jitsi-meet ] && git clone https://github.com/juju4/ansible-role-jitsi-meet $rolesdir/ansible-role-jitsi-meet
 
