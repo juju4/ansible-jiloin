bf43e34862c1e9fcc63a548aeec2693e1f78febf
juju4 <juju4@users.noreply.github.com>
2017-01-07 04:39:19 -0500

    sync repositories - add Jenkinsfile

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,4 +11,10 @@ fi
 
 #[ ! -d $rolesdir/freedomofpress.jitsi-meet ] && git clone https://github.com/freedomofpress/ansible-role-jitsi-meet $rolesdir/freedomofpress.jitsi-meet
 [ ! -d $rolesdir/freedomofpress.jitsi-meet ] && git clone https://github.com/juju4/ansible-role-jitsi-meet $rolesdir/ansible-role-jitsi-meet
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.jiloin ] && ln -s ansible-jiloin $rolesdir/juju4.jiloin
+[ ! -e $rolesdir/juju4.jiloin ] && cp -R $rolesdir/ansible-jiloin $rolesdir/juju4.jiloin
+
+## don't stop build on this script return code
+true
 
